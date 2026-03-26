3d91186d5db59eaa61ddb7c86a15222c886ad902
Andrew Tolpeko <53340454+andytoes@users.noreply.github.com>
2026-03-26 15:41:07 +0300

    Create _worker.js

---
+++ public/_worker.js
@@ -0,0 +1,9 @@
+export default {
+  async fetch(request, env) {
+    const url = new URL(request.url);
+    if (url.pathname.endsWith('/')) {
+      url.pathname += 'index.html';
+    }
+    return env.ASSETS.fetch(url);
+  },
+};
